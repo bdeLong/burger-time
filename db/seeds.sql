7d9da0e24cf259bc51a58127f518c2c514f3c20c
Ben deLong <bendelong45@gmail.com>
2018-12-17 22:47:39 -0600

    Fixed

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('The Krabby Patti');
+INSERT INTO burgers (burger_name) VALUES ('The Big Kahuna Burger');
+INSERT INTO burgers (burger_name) VALUES ('The McTrouble');
